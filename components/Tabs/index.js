0da730f83d096c0a963340c3cc384578a857aefd
Jacqueline <jacquelineomollo@gmail.com>
2019-07-21 22:30:38 -0400

    Started tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,20 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+
+axios.get(` https://lambda-times-backend.herokuapp.com/topics`)
+  .then(data => {
+    console.log('Success! ', data.data);
+
+  })
+
+  .catch(error => {
+    console.log('Error: ', error);
+
+  })
+
+
+
+
